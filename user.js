f9141f127a4bbb73742ef813adea15f5aa629dc2
Tamilanworld <101121374+Tamilanworld@users.noreply.github.com>
2022-04-11 22:43:32 +0530

    middleware-folder

--- user.js
+++ user.js
@@ -1,34 +1,36 @@
-//schema//
-const mongoose = require('mongoose')
-const validator = require('validator');
-
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        trim: true,
-        min: 7,
-        max: 15,
-        unique: true,
-        validate: {
-            validator: function (v) {
-                return /^\w+([\.-]?\w+   )*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
-            },
-            message: "Please enter a valid email"
-        },
-        required: [true, "Email required"]
-    },
-    password: {
-        type: String,
-        required: true,
-        min: 5,
-        max: 10
-
-    }
-})
-module.exports = mongoose.model('user', userSchema)
-
-
+const { check, validationResult } = require('express-validator')
+exports.validatesignup = [
+    check('username')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Name is required')
+        .isAlpha()
+        .withMessage('alpha only')
+        .isLength({ min: 7, max: 15 })
+        .withMessage('min 7 and max 15'),
+    check('email')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('It is not Email pattern'),
+    check('password')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Password Empty')
+        .matches(/^(?=.*[!@#$%^&*])[a-zA-Z0-9 !@#$%^&*]{8,16}$/)
+        .withMessage("must be one special character")
+        // .isNumeric()
+        // .withMessage('Numberic only')
+        // .isLength({ min: 5, max: 10 })
+        // .withMessage('Password must be 5 to 10 characters')
+];
+exports.userValidation = (req, res, next) => {
+    const result = validationResult(req).array();
+    if (!result.length) return next();
+    const error = result[0].msg;
+    res.json({ success: false, message: error });
+};
